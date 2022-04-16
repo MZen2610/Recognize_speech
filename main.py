65b96185cb6b3cdae62513da6e6d7dc5d4b52c91
MZen2610 <mzen1980@gmail.com>
2022-04-16 11:04:40 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
+from google.cloud import dialogflow
+from tg_log_handler import TelegramLogsHandler
 
 import os
 import logging
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -24,41 +19,54 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+def detect_intent_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+def forward_message(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    intent_text = detect_intent_text(
+        context.bot_data['project_id'],
+        update.message.chat_id,
+        update.message.text,
+        'ru-RU'
+    )
+    print(intent_text.query_result.fulfillment_text)
+    update.message.reply_text(intent_text.query_result.fulfillment_text)
 
 
 def main() -> None:
     load_dotenv()
 
     tgm_token = os.environ["TGM_TOKEN"]
+    session_id = os.environ["SESSION_ID"]
+    project_id = os.environ["PROJECT_ID"]
 
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(Bot(token=tgm_token), session_id))
+    logger.info('TG bot running...')
+
     updater = Updater(tgm_token)
 
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
+    dispatcher.bot_data = {'project_id': project_id}
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_message))
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
