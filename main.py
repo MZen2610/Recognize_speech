8e55c9996079ea4b47933c6a205a239913f1101f
MZen2610 <mzen1980@gmail.com>
2022-04-23 10:56:53 +0300

    add DialogFlow to VK bot

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from telegram import Update, ForceReply, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
-from google.cloud import dialogflow
+# from google.cloud import dialogflow
 from tg_log_handler import TelegramLogsHandler
+from handle_intent import detect_intent_texts
 
 import os
 import logging
@@ -19,27 +20,27 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def detect_intent_text(project_id, session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    return response
+# def detect_intent_text(project_id, session_id, text, language_code):
+#     session_client = dialogflow.SessionsClient()
+#     session = session_client.session_path(project_id, session_id)
+#
+#     text_input = dialogflow.TextInput(text=text, language_code=language_code)
+#
+#     query_input = dialogflow.QueryInput(text=text_input)
+#
+#     response = session_client.detect_intent(
+#         request={"session": session, "query_input": query_input}
+#     )
+#     return response
 
 
 def forward_message(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    intent_text = detect_intent_text(
-        context.bot_data['project_id'],
-        update.message.chat_id,
-        update.message.text,
-        'ru-RU'
+    intent_text = detect_intent_texts(
+        project_id=context.bot_data['project_id'],
+        session_id=update.message.chat_id,
+        text=update.message.text,
+        language_code='ru-RU'
     )
     print(intent_text.query_result.fulfillment_text)
     update.message.reply_text(intent_text.query_result.fulfillment_text)
--- vk_bot.py
+++ vk_bot.py
@@ -1,23 +1,39 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
 from dotenv import load_dotenv
+from handle_intent import detect_intent_texts
 
-import vk_api
 import os
+import vk_api as vk
 
-load_dotenv()
 
-vk_token = os.environ["VK_TOKEN"]
+def process_vk_message(event, vk_bot, project_id):
+    answer = detect_intent_texts(
+        project_id=project_id,
+        session_id=event.user_id,
+        text=event.text,
+        language_code='ru-RU'
+    )
+    if not answer.query_result.intent.is_fallback:
+        vk_bot.messages.send(
+            user_id=event.user_id,
+            random_id=get_random_id(),
+            message=answer.query_result.fulfillment_text
+        )
 
-vk_session = vk_api.VkApi(token=vk_token)
-vk_bot = vk_session.get_api()
 
-longpoll = VkLongPoll(vk_session)
+def main():
+    load_dotenv()
+    vk_token = os.environ["VK_TOKEN"]
+    project_id = os.environ["PROJECT_ID"]
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            process_vk_message(event, vk_api, project_id)
+
+
+if __name__ == "__main__":
+    main()
